d30289db2fa52507c90b62c9f3bd39d723ec4d7f
roberthroseroo <34510200+roberthroseroo@users.noreply.github.com>
2018-06-19 19:47:08 +0200

    Add files via upload

---
+++ timer/IRQ_timer.c
@@ -0,0 +1,52 @@
+/*********************************************************************************************************
+**--------------File Info---------------------------------------------------------------------------------
+** File name:           IRQ_timer.c
+** Last modified Date:  2014-09-25
+** Last Version:        V1.00
+** Descriptions:        functions to manage T0 and T1 interrupts
+** Correlated files:    timer.h
+**--------------------------------------------------------------------------------------------------------
+*********************************************************************************************************/
+#include "lpc17xx.h"
+#include "timer.h"
+#include "../Potenciometer/potenciometer.h"
+
+
+/******************************************************************************
+** Function name:		Timer0_IRQHandler
+**
+** Descriptions:		Timer/Counter 0 interrupt handler
+**
+** parameters:			None
+** Returned value:		None
+**
+******************************************************************************/
+
+void TIMER0_IRQHandler (void)
+{
+  LPC_TIM0->IR = 1;			/* clear interrupt flag */
+	/*once the timer is done the A/D converter starts again*/
+	LPC_ADC->ADCR |= (0<<24); // starts the A/D converter
+	LPC_ADC->ADCR |= (1<<24); // starts the A/D converter
+  return;
+}
+
+
+/******************************************************************************
+** Function name:		Timer1_IRQHandler
+**
+** Descriptions:		Timer/Counter 1 interrupt handler
+**
+** parameters:			None
+** Returned value:		None
+**
+******************************************************************************/
+void TIMER1_IRQHandler (void)
+{
+  LPC_TIM1->IR = 1;			/* clear interrupt flag */
+	return;
+}
+
+/******************************************************************************
+**                            End Of File
+******************************************************************************/
---
+++ timer/funct_timer.c
@@ -0,0 +1,58 @@
+/*********************************************************************************************************
+**--------------File Info---------------------------------------------------------------------------------
+** File name:           funct_timer.h
+** Last modified Date:  2014-09-25
+** Last Version:        V1.00
+** Descriptions:        High level functions, possibly exploiting lib_*.c functions
+** Correlated files:    timer.h
+**--------------------------------------------------------------------------------------------------------       
+*********************************************************************************************************/
+#include "lpc17xx.h"
+#include "timer.h"
+
+/*****************************************************************************
+** Function name:		delayMs
+**
+** Descriptions:		Start the timer delay in milo seconds
+**						until elapsed
+**
+** parameters:			timer number, Delay value in milo second			 
+** 						
+** Returned value:		None
+** 
+** Comments [PB]:		Not a very good functions because it is 
+**						keeping the processor control while waiting
+**						count completion
+*****************************************************************************/
+void delayMs(uint8_t timer_num, uint32_t delayInMs)
+{
+  if ( timer_num == 0 )
+  {
+	LPC_TIM0->TCR = 0x02;		/* reset timer */
+	LPC_TIM0->PR  = 0x00;		/* set prescaler to zero */
+	LPC_TIM0->MR0 = delayInMs * (9000000 / 1000-1);
+	LPC_TIM0->IR  = 0xff;		/* reset all interrrupts */
+	LPC_TIM0->MCR = 0x04;		/* stop timer on match */
+	LPC_TIM0->TCR = 0x01;		/* start timer */
+  
+	/* wait until delay time has elapsed */
+	while (LPC_TIM0->TCR & 0x01);
+  }
+  else if ( timer_num == 1 )
+  {
+	LPC_TIM1->TCR = 0x02;		/* reset timer */
+	LPC_TIM1->PR  = 0x00;		/* set prescaler to zero */
+	LPC_TIM1->MR0 = delayInMs * (9000000 / 1000-1);
+	LPC_TIM1->IR  = 0xff;		/* reset all interrrupts */
+	LPC_TIM1->MCR = 0x04;		/* stop timer on match */
+	LPC_TIM1->TCR = 0x01;		/* start timer */
+  
+	/* wait until delay time has elapsed */
+	while (LPC_TIM1->TCR & 0x01);
+  }
+  return;
+}
+
+/******************************************************************************
+**                            End Of File
+******************************************************************************/
